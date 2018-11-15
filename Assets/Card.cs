8cb845049f90764dc86eef77b6d9f74472255cff
Unknown <aea313@nyu.edu>
2018-11-14 19:43:22 -0500

    project added

---
+++ Assets/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card : MonoBehaviour {
+
+	public int value;
+	public Deck.Suit suit;
+	public string id;
+
+	[SerializeField]
+	public ARReferenceImage image;
+
+
+	public Card(int val, Deck.Suit s) {
+		value = val;
+		suit  = s;
+		id    = value + suit.ToString();
+	}
+
+	public static string ImageToId(ARReferenceImage img) {
+		return img.name;
+	}
+
+	// public void ImportImageValues() {
+	// 	value = 
+	// }
+
+}
